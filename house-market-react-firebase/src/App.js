562bba61dee4881728ff2a3a43a3f9a7e0d6dcdc
Umair513 <soomroumair513@gmail.com>
2023-11-18 00:00:23 +0500

    done with basic layout like header layout footer and made necessary pages

--- house-market-react-firebase/src/App.js
+++ house-market-react-firebase/src/App.js
@@ -1,9 +1,22 @@
+import HomePage from "./pages/HomePage";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/offers" element={<Offers></Offers>}></Route>
+        <Route path="/signin" element={<SignIn></SignIn>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/profile" element={<SignIn></SignIn>}></Route>
+      </Routes>
+    </BrowserRouter >
   );
 }
 
---
+++ house-market-react-firebase/src/pages/ForgetPassword.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const ForgetPassword = () => {
+    return (
+        <Layout>
+            <h1>ForgetPassword</h1>
+        </Layout>
+    )
+}
+
+export default ForgetPassword
