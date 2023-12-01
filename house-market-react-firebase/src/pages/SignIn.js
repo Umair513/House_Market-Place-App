464898bac222f613186eca31c1d53e1854b7f8ba
Umair513 <soomroumair513@gmail.com>
2023-12-01 12:38:16 +0500

    done with login logic

--- house-market-react-firebase/src/pages/SignIn.js
+++ house-market-react-firebase/src/pages/SignIn.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { BsFillEyeFill } from "react-icons/bs"
@@ -19,10 +20,22 @@ const SignIn = () => {
             [e.target.id]: e.target.value
         }))
     }
+    const loginHandler = async (e) => {
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            if (userCredential.user) {
+                navigate("/")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <Layout>
             <div className='d-flex align-items-center justify-content-center w-100 mt-4'>
-                <form className='bg-light p-4'>
+                <form className='bg-light p-4' onSubmit={loginHandler}>
                     <h4 className='bg-dark p-2 mt-2 text-light text-center'>Sign In</h4>
 
                     <div className="mb-3">
