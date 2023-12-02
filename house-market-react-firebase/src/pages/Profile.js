1e918f84c3917be82c0d5fa58d74513671db56df
Umair513 <soomroumair513@gmail.com>
2023-12-02 14:11:13 +0500

    Done with User Edit Functionality

--- house-market-react-firebase/src/pages/Profile.js
+++ house-market-react-firebase/src/pages/Profile.js
@@ -8,13 +8,14 @@ import { toast } from 'react-toastify'
 import { db } from "../firebase.config"
 import { FaEdit } from "react-icons/fa"
 import { MdDoneOutline } from "react-icons/md"
+import { doc, updateDoc } from 'firebase/firestore'
 
 const Profile = () => {
     const navigate = useNavigate()
     const auth = getAuth()
     const [formData, setFormData] = useState({
-        // name: auth.currentUser.displayName,
-        // email: auth.currentUser.email
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email
     })
     const { name, email } = formData
 
@@ -24,8 +25,26 @@ const Profile = () => {
         navigate("/")
     }
     const [changeDetails, setChangeDetails] = useState(false)
-    const onsubmit = () => {
-        console.log("click")
+    const onSubmit = async () => {
+        try {
+            if (auth.currentUser.displayName !== name) {
+                await updateProfile(auth.currentUser, {
+                    displayName: name
+                })
+                const userRef = doc(db, "users", auth.currentUser.uid)
+                await updateDoc(userRef, { name })
+                toast.success("User Updated")
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Something went wrong")
+        }
+    }
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }))
     }
     return (
         <Layout>
@@ -37,12 +56,25 @@ const Profile = () => {
                 <div className="card-header">
                     <div className="d-flex justify-content-between">
                         <p>User Personal Details</p>
-                        <span style={{ cursor: "pointer" }} onClick={() => { changeDetails && onsubmit(); setChangeDetails(prevState => !prevState) }}>
+                        <span style={{ cursor: "pointer" }} onClick={() => { changeDetails && onSubmit(); setChangeDetails(prevState => !prevState) }}>
                             {changeDetails ? <MdDoneOutline color='red' /> : <FaEdit color='green'></FaEdit>}
                         </span>
                     </div>
                 </div>
                 <div className="card-body">
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+                            <input type="text" className="form-control" id="name" value={name} onChange={onChange} disabled={!changeDetails} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="email" value={email} onChange={onChange} disabled={!changeDetails} />
+                        </div>
+
+
+
+                    </form>
 
                 </div>
             </div>
